68dac95f63fc2096e8e9476addd8759f37de1d3c
jp-1990 <plum_1990@hotmail.co.uk>
2020-11-11 17:52:31 +0000

    update styling adjustments

--- src/components/Common/Section.js
+++ src/components/Common/Section.js
@@ -1,13 +1,17 @@
 import React from "react"
 
 const Section = ({ name, type, children }) => {
+  const color = {
+    primary: "rgb(0, 123, 145)",
+    secondary: "rgb(0, 172, 202)",
+  }
   const style = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     padding: "50px 0",
     alignItems: "center",
-    backgroundColor: type === "color" ? "rgb(0, 123, 145)" : "white",
+    backgroundColor: type ? color[type] : "white",
     position: "relative",
   }
   return (
--- src/components/Common/SectionTitle.js
+++ src/components/Common/SectionTitle.js
@@ -5,8 +5,28 @@ const SectionTitle = ({ title, side, color, hover }) => {
   let output
   if (title) {
     output = title.split("").map((el, i) => {
-      return <h1 key={i}>{el}</h1>
+      return (
+        <h1 key={i} className={hover ? style[hover] : null}>
+          {el}
+        </h1>
+      )
     })
+  }
+  if (output.length < 8) {
+    const len = 8 - output.length
+    for (let i = 0; i < len; i++) {
+      side === "right"
+        ? output.push(
+            <h1 key={i + 8} style={{ color: "transparent" }}>
+              III
+            </h1>
+          )
+        : output.unshift(
+            <h1 key={i + 8} style={{ color: "transparent" }}>
+              x
+            </h1>
+          )
+    }
   }
   return (
     <div id={title} className={`${style.titleBox} ${style[side]}`}>
