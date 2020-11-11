8d9baed10e63a3046f1f6532aafe0c46b752ca09
jp-1990 <plum_1990@hotmail.co.uk>
2020-11-11 19:39:15 +0000

    update styling reusability improvement

--- src/components/Common/SectionTitle.js
+++ src/components/Common/SectionTitle.js
@@ -12,25 +12,12 @@ const SectionTitle = ({ title, side, color, hover }) => {
       )
     })
   }
-  if (output.length < 8) {
-    const len = 8 - output.length
-    for (let i = 0; i < len; i++) {
-      side === "right"
-        ? output.push(
-            <h1 key={i + 8} style={{ color: "transparent" }}>
-              III
-            </h1>
-          )
-        : output.unshift(
-            <h1 key={i + 8} style={{ color: "transparent" }}>
-              x
-            </h1>
-          )
-    }
-  }
+
   return (
-    <div id={title} className={`${style.titleBox} ${style[side]}`}>
-      <span style={{ color: `${color}` }}>{output}</span>
+    <div id={title} className={style.titleBox}>
+      <span className={style[side]} style={{ color: `${color}` }}>
+        {output}
+      </span>
     </div>
   )
 }
