e4139ec5ea44b4e152fce7a9a6836ce0f331c519
jp-1990 <plum_1990@hotmail.co.uk>
2020-11-11 17:57:01 +0000

    update paths for sections following folder restructure

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
 import ProjectShowcase from "../components/ProjectShowcase/ProjectShowcase"
-import About from "../components/About"
-import Skills from "../components/Skills"
-import Contact from "../components/Contact"
+import About from "../components/About/About"
+import Skills from "../components/Skills/Skills"
+import Contact from "../components/Contact/Contact"
 import SEO from "../components/Seo"
 
 const IndexPage = () => (
