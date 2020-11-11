478782f7ef197f3246065a8b78ee06178d51bd6b
jp-1990 <plum_1990@hotmail.co.uk>
2020-11-11 17:54:09 +0000

    create added basic layout for skills section

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+import Section from "../Common/Section"
+import SectionTitle from "../Common/SectionTitle"
+
+import style from "./Skills.module.scss"
+
+const Skills = () => {
+  return (
+    <div>
+      <Section name="about">
+        <div className={style.containerTop}>
+          <div className={style.wedgeTop}></div>
+        </div>
+        <SectionTitle
+          title="SKILLS"
+          side="right"
+          color=" rgb(180, 180, 180)"
+          hover="secondary"
+        />
+        <div style={{ height: "800px" }}></div>
+        <div className={style.containerBot}>
+          <div className={style.wedgeBot}></div>
+        </div>
+      </Section>
+    </div>
+  )
+}
+
+export default Skills
