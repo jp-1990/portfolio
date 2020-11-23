7d2a6c73912738d58cd690320d9471f3b0392512
jp-1990 <plum_1990@hotmail.co.uk>
2020-11-23 21:10:36 +0000

    update added icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,15 +1,56 @@
 import React from "react"
 
+import { IconContext } from "react-icons"
+import { DiGithubBadge } from "react-icons/di"
+import { SiLinkedin } from "react-icons/si"
+import { VscFilePdf } from "react-icons/vsc"
+
+import testing from "../../downloads/degree.pdf"
+import style from "./Footer.module.scss"
+
 const Footer = () => {
   return (
-    <footer
-      style={{
-        background: `black`,
-        color: "white",
-      }}
-    >
-      <div>
-        <h1>Footer</h1>
+    <footer className={style.footer}>
+      <div className={style.container}>
+        <span>© 2020 J.Plummer. All rights reserved.</span>
+        <div className={style.icons}>
+          <IconContext.Provider
+            value={{ color: "white", verticalAlign: "middle", size: "1.4em" }}
+          >
+            <a
+              href="https://github.com/jp-1990"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <DiGithubBadge />
+            </a>
+          </IconContext.Provider>
+          <IconContext.Provider
+            value={{ color: "white", verticalAlign: "middle" }}
+          >
+            <a
+              href="https://www.linkedin.com/in/james-plummer-73267712b/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiLinkedin />
+            </a>
+          </IconContext.Provider>
+          <div className={style.cv}>
+            <a href={testing} target="_blank" rel="noreferrer">
+              <IconContext.Provider
+                value={{
+                  color: "white",
+                  verticalAlign: "middle",
+                  size: "0.8em",
+                }}
+              >
+                <VscFilePdf />
+              </IconContext.Provider>
+              <span>CV</span>
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   )
